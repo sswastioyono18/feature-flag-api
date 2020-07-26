3b2fb064f50870520ec762888b9d1bf6e192c30a
Sactio Swastioyono <email.tyo@gmail.com>
2020-07-26 20:39:00 +0900

    Add circleci and codecov

---
+++ src/test/kotlin/controller/FeatureFlagControllerITVersion2.kt
@@ -0,0 +1,81 @@
+package controller
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.project.featureflag.FeatureFlagApplication
+import com.project.featureflag.config.MySQLBaseContainer
+import com.project.featureflag.config.MySQLContextInitializer
+import com.project.featureflag.entity.FeatureFlagEntity
+import com.project.featureflag.repository.FeatureFlagRepository
+import org.junit.Before
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import java.util.*
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(initializers = [MySQLContextInitializer::class], classes = [FeatureFlagApplication::class])
+@AutoConfigureMockMvc
+class FeatureFlagControllerITVersion2 {
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+    @Autowired
+    lateinit var objectMapper: ObjectMapper
+
+    @Autowired
+    lateinit var featureFlagRepository : FeatureFlagRepository
+
+
+    @BeforeEach
+    fun setup() {
+        var featureFlagEntity = FeatureFlagEntity()
+        featureFlagEntity.applicationName = "test"
+        featureFlagEntity.creationDateTime = Date()
+        featureFlagEntity.version = "1.10"
+        featureFlagRepository.save(featureFlagEntity)
+    }
+
+    @Test
+    fun `assert call feature flag return app name with version`() {
+
+        val mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/feature/test").contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful)
+                .andReturn()
+
+        val c: FeatureFlagEntity = objectMapper.readValue(mvcResult.response.contentAsString)
+        assertEquals(c.applicationName, "test")
+        assertEquals(c.version, "1.10")
+
+    }
+
+    @Test
+    fun `assert call feature not found return RecordNotFoundException`() {
+        var featureFlagEntity = FeatureFlagEntity()
+        featureFlagEntity.applicationName = "test123"
+        featureFlagEntity.creationDateTime = Date()
+        featureFlagEntity.version = "1.10"
+        featureFlagRepository.save(featureFlagEntity)
+        val mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/feature/test1").contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is4xxClientError)
+                .andReturn()
+
+        assertEquals(mvcResult.resolvedException?.message, "test1 does not exist in DB")
+
+    }
+
+}
